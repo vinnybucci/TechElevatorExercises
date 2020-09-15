8096797dc320dbc343e2df40cc5486ba0861d8ae
Tom Anderson <tom.anderson@techelevator.com>
2020-09-15 08:38:12 -0400

    Today's info

---
+++ module-1/02_Variables_Data_Types/student-breakout/01_DeclarationInitialization/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DeclarationInitialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* VARIABLES & DATA TYPES */
+
+            /*
+		    1. Create a variable to hold an int and call it numberOfExercises.
+			Then set it to 26.
+		    */
+            int numberOfExercises;
+            numberOfExercises = 26;
+            Console.WriteLine(numberOfExercises);
+
+            /*
+            2. Create a variable to hold a double and call it half.
+                Set it to 0.5.
+            */
+
+            //Console.WriteLine(half);
+
+            /*
+            3. Create a variable to hold a string and call it name.
+                Set it to "TechElevator".
+            */
+
+            //Console.WriteLine(name);
+
+            /*
+            4. Create a variable called seasonsOfFirefly and set it to 1.
+            */
+
+            //Console.WriteLine(seasonsOfFirefly);
+
+            /*
+            5. Create a variable called myFavoriteLanguage and set it to "C#".
+            */
+
+            //Console.WriteLine(myFavoriteLanguage);
+
+            /*
+            6. Create a variable called pi and set it to 3.1416.
+            */
+
+            //Console.WriteLine(pi);
+
+            /*
+            7. Create and set a variable that holds your name. Write it out to the console.
+            */
+
+            /*
+            8. Create and set a variable that holds the number of buttons on your mouse. Write it out to the console.
+            */
+
+            /*
+            9. Create and set a variable that holds the percentage of battery left on
+            your phone. Write it out to the console.
+            */
+        }
+    }
+}
