12545ed148828cb5541b60e1e5632d4fc66649f4
Tom Anderson <tom.anderson@techelevator.com>
2020-09-16 12:57:37 -0400

    Today's info

---
+++ module-1/03_Expressions/lecture-final/Lecture/15_ReturnAdultOrMinorAgain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureExample
+    {
+
+        /*
+        15. Now, do it again with a different bool operation.
+        TOPIC: Logical Not
+        */
+        public string ReturnAdultOrMinorAgain(int number)
+        {
+            if (!(number < 18))
+            {
+                return "Adult";
+            }
+            else
+            {
+                return "Minor";
+            }
+        }
+    }
+}
---
+++ module-1/03_Expressions/lecture-final/Lecture/16_ReturnTrueWhenLargeEvenNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+        16. Return "Big Even Number" when number is even, larger than 100, and a multiple of 5.
+            Return "Big Number" if the number is just larger than 100.
+            Return empty string for everything else.
+            TOPIC: Complex Expression
+        */
+        public string ReturnBigEvenNumber(int number)
+        {
+            string output = "";
+            if(number != 0 && 2 / number == 1 && number > 100 && number % 5 == 0)
+            {
+                output = "Big Even Number";
+            } else if(number > 100)
+            {
+                output = "Big Number";
+            }
+            return output;
+        }
+        
+
+
+    }
+}
