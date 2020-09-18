98fbb86b4a9827e089d2ebadf1745fb253720056
Tom Anderson <tom.anderson@techelevator.com>
2020-09-18 12:03:31 -0400

    Today's info

---
+++ module-1/05_CommandLine_Programs/lecture-final/DiscountCalculator/DiscountCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DiscountCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Discount Calculator");
+            // Prompt the user for the price
+            Console.Write("Enter the discount price (w/o percent sign): ");
+            decimal discount = decimal.Parse(Console.ReadLine()) / 100;
+
+            // Prompt them for the price of the item
+            Console.Write("Please enter the prices separated by spaces. ");
+            string prices = Console.ReadLine();
+            char[] delimiters = { ' ', ',', ';' };
+            // StringSplitOptions.RemoveEmptyEntries removes any empty entries
+            string[] priceArray = prices.Split(delimiters,StringSplitOptions.RemoveEmptyEntries);
+
+            // Loop through the array
+            for(int i = 0; i < priceArray.Length; i++)
+            {
+                // convert the string value in the array to a decimal
+                decimal price = decimal.Parse(priceArray[i]);
+                // calculated the values
+                decimal amountOff = price * discount;
+                decimal salePrice = price - amountOff;
+                // wrote them to the screen.
+                Console.WriteLine($"Original Price: {price:C2} | Sale Price: {salePrice:C2}");
+
+            }
+        }
+    }
+}
