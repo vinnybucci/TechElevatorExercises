57412d84046217771db86d9271e6b7e6655a9f0b
Tom Anderson <tom.anderson@techelevator.com>
2020-09-21 12:25:12 -0400

    Today's info

---
+++ module-1/06_Introduction_Objects_Strings/student-exercise/Exercises/16_EndsLy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public partial class StringExercises
+    {
+        /*
+         Given a string, return true if it ends in "ly".
+         EndsLy("oddly") → true
+         EndsLy("y") → false
+         EndsLy("oddy") → false
+         */
+        public bool EndsLy(string str)
+        {
+            return false;
+        }
+    }
+}
