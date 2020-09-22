11d31fa00e6bcc8f3a7ccc62a70dedff1a7c0590
Tom Anderson <tom.anderson@techelevator.com>
2020-09-22 08:20:58 -0400

    Today's info

---
+++ module-1/07_Collections_Part_1/student-breakouts/01_Lists/CollectionsLectureNotes/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsLectureNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LIST<T>
+            //
+            // Lists allow us to hold collections of data. They are declared with a type of data that they hold
+            // only allowing items of that type to go inside of them.
+            //
+            // The syntax used for declaring a new list of type T is
+            //      List<T> list = new List<T>();
+            //
+            //
+
+            // Create two lists of integers
+
+
+            // Create list of strings
+
+            // Write these variables to the console
+
+            // Discuss: What did you see on the console? Is that what you expected?
+
+            /////////////////
+
+
+            //////////////////
+            // OBJECT EQUALITY
+            //////////////////
+
+            // Check if the first list you created is equal to the second list
+            // If they are equal, write "They are the same" to the console.
+            // If they are not equal, write "They are not the same" to the console.
+            // Discuss: Why did you get that result?
+
+            // Assign the first integer list to the second integer list
+
+            // Check if the first list you created is equal to the second list
+            // If they are equal, write "They are the same" to the console.
+            // If they are not equal, write "They are not the same" to the console.
+            // Discuss: Why did you get that result?
+
+
+            /////////////////
+            // ADDING ITEMS
+            /////////////////
+
+            // Add three numbers to one of the integer lists
+
+            // Add four words to the list of strings
+
+
+            //////////////////
+            // ACCESSING BY INDEX
+            //////////////////
+            // Use a for loop to access each element by its index
+            // Write each element to the console. 
+            // Do this for both the integer list and the string list.
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
