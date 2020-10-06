fec44c490f328f7e1aac429cba1917daf1acc10c
Vinny Bucci <vinnybucci10@gmail.com>
2020-10-06 18:16:58 -0400

    Exercise completed

--- module-1/17_File_Writing/student-exercise/FizzWriter/Program.cs
+++ module-1/17_File_Writing/student-exercise/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,46 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string directory = Environment.CurrentDirectory;
+            string fileName = "FizzBuzz.txt";
+            string fullPath = Path.Combine(directory, fileName);
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fullPath, true))
+                {
+                    string result;
+                    for (int i = 0; i <= 300; i++)
+                    {
+                        if (i % 3 == 0 && i % 5 == 0)
+                        {
+                            result = "FizzBuzz";
+                        }
+                        else if (i % 3 == 0)
+                        {
+                            result = "Fizz";
+                        }
+                        else if (i % 5 == 0)
+                        {
+                            result = "Buzz";
+                        }
+                        else
+                        {
+                            result = i.ToString();
+                        }
+                            sw.WriteLine(result);
+
+
+
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
         }
     }
 }
