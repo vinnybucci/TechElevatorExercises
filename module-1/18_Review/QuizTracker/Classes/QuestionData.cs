d0a6b4d9f2d559d3cce46ab3fb455786982f3493
Tom Anderson <tom.anderson@techelevator.com>
2020-10-07 11:50:58 -0400

    Today's info

---
+++ module-1/18_Review/QuizTracker/Classes/QuestionData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizTracker.Classes
+{
+    public class QuestionData
+    {
+        public string QuizTitle { get; set; }
+        public DateTime QuizDate { get; set; }
+        public double Score { get; set; } = 100;
+        public int QuestionNumber { get; set; }
+
+        public QuestionData() { }
+        public QuestionData(string csv)
+        {
+            try
+            {
+                string[] data = csv.Split(',',StringSplitOptions.RemoveEmptyEntries);
+                QuizTitle = data[0];
+                QuizDate = DateTime.Parse(data[1]);
+                Score = int.Parse(data[2]);
+                QuestionNumber = int.Parse(data[3]);
+            } catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        public string PrepData()
+        {
+            return $"{QuizTitle},{QuizDate},{Score},{QuestionNumber}";
+        }
+        public override string ToString()
+        {
+            return $"On {QuizDate} we scored {Score} on question number {QuestionNumber} of quiz {QuizTitle}.";
+        }
+    }
+}
