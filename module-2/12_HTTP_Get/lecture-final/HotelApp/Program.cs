6566c2273df093e632dc995970dffaaad1fe8a60
Toma_TE <tom.anderson@techelevator.com>
2020-10-27 11:54:56 -0400

    Today's info

---
+++ module-2/12_HTTP_Get/lecture-final/HotelApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using HTTP_Web_Services_GET_lecture.Data;
+using System;
+using System.Collections.Generic;
+
+namespace HTTP_Web_Services_GET_lecture
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            APIService apiService = new APIService("http://localhost:3000/");
+            CLI cli = new CLI(apiService);
+            cli.Run();
+        }
+
+    }
+}
