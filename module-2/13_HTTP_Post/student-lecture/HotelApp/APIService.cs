4dae8cfb542ee37a0838f52f78e190eff1f22bd8
Toma_TE <tom.anderson@techelevator.com>
2020-10-28 08:47:07 -0400

    Today's info

---
+++ module-2/13_HTTP_Post/student-lecture/HotelApp/APIService.cs
@@ -0,0 +1,95 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace HTTP_Web_Services_POST_PUT_DELETE_lecture
+{
+    class APIService
+    {
+        private readonly string API_URL = "";
+        private readonly RestClient client = new RestClient();
+
+        public APIService(string api_url)
+        {
+            API_URL = api_url;
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            RestRequest request = new RestRequest(API_URL + "hotels");
+            IRestResponse<List<Hotel>> response = client.Get<List<Hotel>>(request);
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                Console.WriteLine("Error occurred - unable to reach server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                Console.WriteLine("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+            else
+            {
+                return response.Data;
+            }
+            return null;
+        }
+
+        public List<Reservation> GetReservations(int hotelId = 0)
+        {
+            string url = API_URL;
+            if (hotelId != 0)
+                url += $"hotels/{hotelId}/reservations";
+            else
+                url += "reservations";
+
+            RestRequest request = new RestRequest(url);
+            IRestResponse<List<Reservation>> response = client.Get<List<Reservation>>(request);
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                Console.WriteLine("Error occurred - unable to reach server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                Console.WriteLine("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+            else
+            {
+                return response.Data;
+            }
+            return null;
+        }
+
+        public Reservation GetReservation(int reservationId)
+        {
+            RestRequest request = new RestRequest(API_URL + "reservations/" + reservationId);
+            IRestResponse<Reservation> response = client.Get<Reservation>(request);
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                Console.WriteLine("Error occurred - unable to reach server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                Console.WriteLine("Error occurred - received non-success response: " + (int)response.StatusCode);
+            }
+            else
+            {
+                return response.Data;
+            }
+            return null;
+        }
+
+        public Reservation AddReservation(Reservation newReservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation UpdateReservation(Reservation reservationToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteReservation(int reservationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ module-2/13_HTTP_Post/tutorial-final/LocationApp/APIService.cs
@@ -0,0 +1,134 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace LocationApp
+{
+    public class APIService
+    {
+        const string API_URL = "http://localhost:3000/locations";
+        readonly RestClient client = new RestClient();
+
+        public List<Location> GetAllLocations()
+        {
+            RestRequest request = new RestRequest(API_URL);
+            IRestResponse<List<Location>> response = client.Get<List<Location>>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                //response not received
+                Console.WriteLine("An error occurred communicating with the server.");
+                return null;
+            }
+            else if (!response.IsSuccessful)
+            {
+                //response non-2xx
+                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                return null;
+            }
+            else
+            {
+                //success
+                return response.Data;
+            }
+        }
+
+        public Location GetDetailsForLocation(int locationId)
+        {
+            RestRequest requestOne = new RestRequest(API_URL + "/" + locationId);
+            IRestResponse<Location> response = client.Get<Location>(requestOne);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                //response not received
+                Console.WriteLine("An error occurred communicating with the server.");
+                return null;
+            }
+            else if (!response.IsSuccessful)
+            {
+                //response non-2xx
+                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                return null;
+            }
+            else
+            {
+                //success
+                return response.Data;
+            }
+        }
+
+        public Location AddLocation(Location newLocation)
+        {
+            RestRequest request = new RestRequest(API_URL);
+            request.AddJsonBody(newLocation);
+            IRestResponse<Location> response = client.Post<Location>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                //response not received
+                Console.WriteLine("An error occurred communicating with the server.");
+                return null;
+            }
+            else if (!response.IsSuccessful)
+            {
+                //response non-2xx
+                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                return null;
+            }
+            else
+            {
+                //success
+                Console.WriteLine("Location successfully added");
+                return response.Data;
+            }
+        }
+
+        public Location UpdateLocation(Location locationToUpdate)
+        {
+            RestRequest request = new RestRequest(API_URL + "/" + locationToUpdate.Id);
+            request.AddJsonBody(locationToUpdate);
+            IRestResponse<Location> response = client.Put<Location>(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                //response not received
+                Console.WriteLine("An error occurred communicating with the server.");
+                return null;
+            }
+            else if (!response.IsSuccessful)
+            {
+                //response non-2xx
+                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                return null;
+            }
+            else
+            {
+                //success
+                Console.WriteLine("Location successfully updated");
+                return response.Data;
+            }
+        }
+
+        public void DeleteLocation(int locationId)
+        {
+            RestRequest request = new RestRequest(API_URL + "/" + locationId);
+            IRestResponse response = client.Delete(request);
+
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                //response not received
+                Console.WriteLine("An error occurred communicating with the server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                //response non-2xx
+                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+            }
+            else
+            {
+                //success
+                Console.WriteLine("Location successfully deleted");
+            }
+        }
+    }
+}
