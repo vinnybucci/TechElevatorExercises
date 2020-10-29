a568b2458fe2d6add88f68363427dec9b1987ff9
Vinny Bucci <vinnybucci10@gmail.com>
2020-10-29 16:15:46 -0400

    working on exercise price

--- module-2/14_ServerSide_API_Part1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
+++ module-2/14_ServerSide_API_Part1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using AuctionApp.Models;
 using AuctionApp.DAO;
+using System.Reflection.Metadata;
 
 namespace AuctionApp.Controllers
 {
@@ -22,5 +23,39 @@ namespace AuctionApp.Controllers
                 dao = auctionDao;
             }
         }
+        
+        [HttpGet("{id}")]
+        public Auction GetAuctionsId(int id)
+        {
+            Auction auction = dao.Get(id);
+            if (auction != null)
+            {
+            return auction;
+
+            }
+            return null;
+        }
+        [HttpGet]
+        public List<Auction> SearchTitle(string title_like = "")
+        {
+            List<Auction> output = dao.SearchByTitle(title_like);
+            return output;
+            
+
+        }
+        //[HttpGet]
+        //public List<Auction> SearchPrice(double currentBid_lte = 0)
+        //{
+        //    List<Auction> output = dao.SearchByPrice(currentBid_lte);
+        //    return output;
+
+        //}
+        [HttpPost]
+        public Auction PostAuctions(Auction auction)
+        {
+            Auction output = dao.Create(auction);
+            return output;
+        }
+
     }
 }
