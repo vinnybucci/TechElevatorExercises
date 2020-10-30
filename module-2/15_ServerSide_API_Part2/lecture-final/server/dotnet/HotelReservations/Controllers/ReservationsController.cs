f814b5dd58ca7a421c75d41b6792118ba84ddce9
Toma_TE <tom.anderson@techelevator.com>
2020-10-30 11:57:02 -0400

    New material

---
+++ module-2/15_ServerSide_API_Part2/lecture-final/server/dotnet/HotelReservations/Controllers/ReservationsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelReservations.Dao;
+using HotelReservations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private static IReservationDao reservationDao;
+
+        public ReservationsController(IReservationDao _reservationDao)
+        {
+            reservationDao = _reservationDao;
+        }
+
+        [HttpGet]
+        public List<Reservation> ListReservations()
+        {
+            return reservationDao.List();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> GetReservation(int id)
+        {
+            Reservation reservation = reservationDao.Get(id);
+
+            if (reservation != null)
+            {
+                return reservation;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Reservation> AddReservation(Reservation reservation)
+        {
+            if(reservation.CheckinDate == null)
+            {
+                return BadRequest();
+            }
+            Reservation added = reservationDao.Create(reservation);
+            return Created($"/reservations/{added.Id}", added);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Reservation> UpdateReseravtion(int id, Reservation reservation)
+        {
+            Reservation existingReservation = reservationDao.Get(id);
+            if (existingReservation == null)
+            {
+                return NotFound($"Reservation id {id} doesn't exist");
+            }
+
+            Reservation result = reservationDao.Update(id, reservation);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ module-2/15_ServerSide_API_Part2/lecture-final/server/dotnet/HotelReservations/Models/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservations.Models
+{
+    public class Reservation
+    {
+        public int? Id { get; set; }
+        [Required]
+        public int HotelID { get; set; }
+        [Required(ErrorMessage ="Please provide a full name for the reservation")]
+        public string FullName { get; set; }
+        [Required]
+        public string CheckinDate { get; set; }
+        [Required]
+        public string CheckoutDate { get; set; }
+        [Required]
+        [Range(1,5,ErrorMessage ="Our Rooms hold a maximum of 5 people")]
+        public int Guests { get; set; }
+
+        public Reservation(int? id, int hotelId, string fullName, string checkinDate, string checkoutDate, int guests)
+        {
+            Id = id ?? new Random().Next(100, int.MaxValue);
+            HotelID = hotelId;
+            FullName = fullName;
+            CheckinDate = checkinDate;
+            CheckoutDate = checkoutDate;
+            Guests = guests;
+        }
+    }
+}
