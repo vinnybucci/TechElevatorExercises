a8f403826c1b2dca72e2931fac34ba7eab0e1b8f
Toma_TE <tom.anderson@techelevator.com>
2020-11-03 12:09:01 -0500

    New material

---
+++ module-2/17_Review/lecture-final/CatCards/Services/CatFactService.cs
@@ -0,0 +1,29 @@
+﻿using CatCards.Models;
+using RestSharp;
+using System.Net.Http;
+
+namespace CatCards.Services
+{
+    public class CatFactService : ICatFactService
+    {
+        private readonly string API_URL = "https://cat-fact.herokuapp.com/facts/random";
+        private readonly RestClient client = new RestClient();
+
+        public CatFact GetFact()
+        {
+            RestRequest request = new RestRequest(API_URL);
+            IRestResponse<CatFact> response = client.Get<CatFact>(request);
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                throw new HttpRequestException("Could not contact server, please try again later.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException(response.StatusCode.ToString());
+            } else
+            {
+                return response.Data;
+            }
+        }
+    }
+}
