c5f57673d8a1ccf225bd4fa8f54c8d386d3a76b7
Toma_TE <tom.anderson@techelevator.com>
2020-11-09 08:42:50 -0500

    New material

---
+++ module-2/16_Authentication/exercise-final/AuctionAppClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace AuctionApp
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ConsoleService console = new ConsoleService();
+            console.Run();
+        }
+
+    }
+}
