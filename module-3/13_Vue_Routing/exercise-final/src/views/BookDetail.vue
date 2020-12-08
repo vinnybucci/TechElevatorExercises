c47ac10c3957d5bdb4aad67504db6841ebb9b872
Toma_TE <tom.anderson@techelevator.com>
2020-12-08 13:10:38 -0500

    New material

---
+++ module-3/13_Vue_Routing/exercise-final/src/views/BookDetail.vue
@@ -0,0 +1,39 @@
+<template>
+  <book-details v-bind:book="viewedBook" />
+</template>
+
+<script>
+import BookDetails from '@/components/BookDetails.vue';
+
+export default {
+  components: {
+      BookDetails
+  },
+  computed: {
+    viewedBook() {
+      let thisBook = this.$store.state.books.filter((book) => {
+        return book.isbn == this.$route.params.id;
+      });
+      if (thisBook.length > 0) {
+        return thisBook[0]; //return first element
+      } else {
+        return { //return empty book object
+          title: '',
+          author: '',
+          read: false,
+          isbn: ''
+        };
+      }
+
+      //can also be done with .find, but the students haven't been taught this one
+      // return this.$store.state.books.find((book) => {
+      //   return book.isbn == this.$route.params.id;
+      // });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ module-3/13_Vue_Routing/exercise-final/src/views/MyBooks.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <reading-list />
+  <router-link v-bind:to="{ name: 'newBook' }">Add Book to Reading List</router-link>
+</div>
+</template>
+
+<script>
+import ReadingList from '@/components/ReadingList.vue';
+
+export default {
+  components: {
+      ReadingList
+  }
+}
+</script>
+
+<style>
+
+</style>
